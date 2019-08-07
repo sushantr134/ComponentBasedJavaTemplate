5ddd98deb56ba505717cbb385e809e416ddcf2e6
abhishek chauhan <abhishek98.ac@gmail.com>
2019-08-08 04:22:53 +0530

    APUOilTank jar generated

--- component/src/APUOilTank.java
+++ component/src/APUOilTank.java
@@ -1,2 +1,44 @@
 public class APUOilTank {
+    private static APUOilTank instance = new APUOilTank();
+    public Port port;
+    private String manufacturer = "<Abhishek Chauhan>";
+    private String type="team<28>";
+    private String id = "<161B294>";
+    private int level=0;
+    public APUOilTank(){
+        port = new Port();
+    }
+    public static APUOilTank getinstance()
+    {
+        return instance;
+    }
+    public String innerVersion(){
+        return "APUOilTank//"+ manufacturer+ "-"+type+ "-"+id;
+    }
+    public int innerIncreaseLevel(){
+        level++;
+        return level;
+    }
+    public int innerDecreaseLevel(){
+        level--;
+        return level;
+    }
+    public class Port implements IAPUOilTank{
+        @Override
+        public String version() {
+            return innerVersion();
+        }
+
+        @Override
+        public int increase(int value) {
+            return innerIncreaseLevel();
+        }
+
+        @Override
+        public int decrease(int value) {
+            return innerDecreaseLevel();
+        }
+
+    }
+
 }
--- component/src/IAPUOilTank.java
+++ component/src/IAPUOilTank.java
@@ -1,2 +1,6 @@
 public interface IAPUOilTank {
+    String version();
+    int increase(int value);
+    int decrease(int value);
+
 }
