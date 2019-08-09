bd8b575710de8ae1c38155b942bbe8f5ff2a120b
abhishek chauhan <abhishek98.ac@gmail.com>
2019-08-09 16:21:52 +0530

    Events done

---
+++ src/event/apu_oil_tank/APUOilTankDecreaseLevel.java
@@ -0,0 +1,11 @@
+package event.apu_oil_tank;
+
+public class APUOilTankDecreaseLevel {
+    int level;
+    public APUOilTankDecreaseLevel(int level) {
+        this.level = level;
+    }
+    public String toString() {
+        return "Event-APUOilTank - DecreaseLevel";
+    }
+}
---
+++ src/event/apu_oil_tank/APUOilTankIncreaseLevel.java
@@ -0,0 +1,11 @@
+package event.apu_oil_tank;
+
+public class APUOilTankIncreaseLevel {
+    int level;
+    public APUOilTankIncreaseLevel(int level) {
+        this.level = level;
+    }
+    public String toString() {
+        return "Event-APUOilTank - IncreaseLevel";
+    }
+}
